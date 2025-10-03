225aca319b54cea365ce48d4e61802e8dc0e7627
davy39 <davy@ik.me>
2025-10-03 23:25:03 +0200

    formatting with fmt

--- worker/x_worker.ml
+++ worker/x_worker.ml
@@ -6,20 +6,13 @@ type protocol_type = Ocaml_Request | Json_Request
 let respond protocol m =
   match protocol with
   | Json_Request ->
-      Worker.post_message 
-        (Js.string 
-          (X_ocaml_json.json_string_of_response m))
-  | Ocaml_Request -> 
-      Worker.post_message 
-        (X_protocol.resp_to_bytes m)
+      Worker.post_message (Js.string (X_ocaml_json.json_string_of_response m))
+  | Ocaml_Request -> Worker.post_message (X_protocol.resp_to_bytes m)
 
 let reformat protocol ~id code =
-  let code' = 
-    try Ocamlfmt.fmt code 
-    with err -> code in
-    if code <> code' then 
-      respond protocol (Formatted_source (id, code'));
-    code'
+  let code' = try Ocamlfmt.fmt code with err -> code in
+  if code <> code' then respond protocol (Formatted_source (id, code'));
+  code'
 
 let run () =
   Js_of_ocaml.Worker.set_onmessage @@ fun (msg : 'a Js.t) ->
@@ -37,18 +30,17 @@ let run () =
   in
   match request with
   | protocol, message -> (
-    match message with
-    | Merlin (id, action) ->
-        respond protocol
-          (Merlin_response (id, Merlin_worker.on_message action))
-    | Format_config conf -> 
-        Ocamlfmt.configure conf
-    | Format (id, code) -> 
-        ignore (reformat protocol ~id code : string)
-    | Eval (id, line_number, code) ->
-        let code = reformat protocol ~id code in
-        let output ~loc out =
-          respond protocol (Top_response_at (id, loc, out)) in
-        let result = Eval.execute ~output ~id ~line_number code in
-        respond protocol (Top_response (id, result))
-    | Setup -> Eval.setup_toplevel ())
+      match message with
+      | Merlin (id, action) ->
+          respond protocol
+            (Merlin_response (id, Merlin_worker.on_message action))
+      | Format_config conf -> Ocamlfmt.configure conf
+      | Format (id, code) -> ignore (reformat protocol ~id code : string)
+      | Eval (id, line_number, code) ->
+          let code = reformat protocol ~id code in
+          let output ~loc out =
+            respond protocol (Top_response_at (id, loc, out))
+          in
+          let result = Eval.execute ~output ~id ~line_number code in
+          respond protocol (Top_response (id, result))
+      | Setup -> Eval.setup_toplevel ())
